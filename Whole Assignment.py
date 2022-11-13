b885bf489c260e411216c6ccdc9dc43e4cff7960
Biki <102693150+Bikash1075@users.noreply.github.com>
2022-11-13 21:49:33 +0530

    Add files via upload

--- Whole Assignment.py
+++ Whole Assignment.py
@@ -13,7 +13,7 @@ class Point:
 a=Point(1,3,5)
 print(a.SqSum())
 print("---------------------------------------------")
-
+print("---------------------------------------------")
 
 # Challenge 2
 # Calculator
@@ -35,7 +35,7 @@ print(c.substract())
 print(c.multiply())
 print(c.divide())
 print("---------------------------------------------")
-
+print("---------------------------------------------")
 
 # Challenge 3
 # Complete Student Class
@@ -54,7 +54,7 @@ print(s.getName())
 s.setRollNumber(75)
 print(s.getRollNumber())
 print("---------------------------------------------")
-
+print("---------------------------------------------")
 
 # Challenge 4
 # Parent and child class
@@ -69,19 +69,22 @@ class SavingsAccount(Account):
         self.title=title
         self.balance=balance
         self.InterestRate=InterestRate
+
+print("Account Details")
 A=Account("Asish", 5000)
 print(f"Name of the Account Holder is {A.title}")
 print(f"Available Balance is {A.balance} Rs")
 print(f"Here, {A.title} is the title, {A.balance} is the balance . ")
-SA=SavingsAccount("Asish", 5000, 5)
 print("--------------------------------------------------------------")
+print("Savings Account Details")
+SA=SavingsAccount("Asish", 5000, 5)
 print(f"Name of the Account Holder is {SA.title}")
 print(f"Available Balance is {SA.balance} Rs")
 print(f"Name of the Account Holder is {A.title}")
 print(f"Interest Rate on Current Balance in percent is {SA.InterestRate}")
 print(f"Here, {SA.title} is the title, {SA.balance} is the balance and {SA.InterestRate} is the interestRate .")
 print("---------------------------------------------")
-
+print("---------------------------------------------")
 
 # Challenge 5
 # handling bank account
