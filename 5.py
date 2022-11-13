4ab8a4d55b3a4562acf71f766558f3b72f9f7894
Biki <102693150+Bikash1075@users.noreply.github.com>
2022-11-13 21:51:43 +0530

    Add files via upload

--- 5.py
+++ 5.py
@@ -36,6 +36,29 @@ Si=SavingsAccount("Asish",2000,5)
 print(Si.interestAmount())
 print("---------------------------------------------")
 
+# 2nd approach
+class Account:
+    def __init__(self, title, balance):
+        self.title = title
+        self.balance = balance
+    
+    def withdrawal(self, amount):
+        self.balance=self.balance - amount
+        
+    
+    def deposit(self, amount):
+       self.balance=self.balance + amount 
+       
+    def getBalance(self):
+        return self.balance
+
+class SavingsAccount(Account):
+    def __init__(self, title=None, balance=0, interestRate=0):
+            super().__init__(title, balance)
+            self.interestRate = interestRate
+    
+    def interestAmount(self):
+        return (self.balance*self.interestRate)/100
 Ad=Account("Asish",2000)
 Ad.deposit(500)
 Ad.getBalance()
